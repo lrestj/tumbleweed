2e88e6b9b786632e6ff80342f4a4b1e023b00e04
libor <liborest06@gmail.com>
2025-06-12 15:17:10 +0200

    Brave install script added

---
+++ .dotfiles/scripts/braveinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##### Install Brave browser #####
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg &&
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
+
+sudo apt update
+
+sudo apt install brave-browser
+
+##### END OF FILE #####
