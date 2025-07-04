05c7bb7986680160e141c08b45365ef40d4540ec
LrestJ <rest@seznam.cz>
2025-07-04 11:44:41 +0200

    Apt-listbugs added

--- .dotfiles/scripts/installdeb.sh
+++ .dotfiles/scripts/installdeb.sh
@@ -2,7 +2,7 @@
 
 ##### Debian Hyprland install #####
 
-Apps="ark breeze brightnessctl btop cliphist eog evince fastfetch figlet fish fonts-noto fonts-font-awesome foot geany gnome-calculator gnome-disk-utility grim gthumb gvfs-backends hyprland hyprland-protocols hyprwayland-scanner jq libfuse2 libglib2.0-bin libnotify-bin lxqt-policykit mako-notifier network-manager-applet nfs-common nwg-look pamixer power-profiles-daemon qt6ct slurp swaybg swayidle swaylock thunar thunar-archive-plugin udiskie vainfo waybar wl-clipboard wlogout wlsunset wofi xournalpp xwayland"
+Apps="apt-listbugs ark breeze brightnessctl btop cliphist eog evince fastfetch figlet fish fonts-noto fonts-font-awesome foot geany gnome-calculator gnome-disk-utility grim gthumb gvfs-backends hyprland hyprland-protocols hyprwayland-scanner jq libfuse2 libglib2.0-bin libnotify-bin lxqt-policykit mako-notifier network-manager-applet nfs-common nwg-look pamixer power-profiles-daemon qt6ct slurp swaybg swayidle swaylock thunar thunar-archive-plugin udiskie vainfo waybar wl-clipboard wlogout wlsunset wofi xournalpp xwayland"
 
 
 sudo apt update && sudo apt upgrade
