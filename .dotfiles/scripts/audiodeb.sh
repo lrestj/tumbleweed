2f0f108bd39beaae88a9aa0f5f288cad36224340
LrestJ <rest@seznam.cz>
2025-07-04 16:47:57 +0200

    Fixes

---
+++ .dotfiles/scripts/audiodeb.sh
@@ -0,0 +1,10 @@
+#/bin/bash/
+
+sudo apt update &&
+sudo apt install pipewire pipewire-audio-client-libraries wireplumber &&
+sudo apt install pavucontrol &&
+
+systemctl --user enable pipewire.service
+systemctl --user start pipewire.service
+systemctl --user enable wireplumber.service
+systemctl --user start wireplumber.service
