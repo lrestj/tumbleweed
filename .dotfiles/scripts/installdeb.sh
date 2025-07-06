efe35856e608401dc34041b6a49e12faaef02e65
LrestJ <rest@seznam.cz>
2025-07-06 11:03:33 +0200

    Install script update

--- .dotfiles/scripts/installdeb.sh
+++ .dotfiles/scripts/installdeb.sh
@@ -22,38 +22,17 @@ echo -e "\n"
 echo "Nastavení swap"
 echo vm.swappiness=10 | sudo tee /etc/sysctl.d/99-swappiness.conf
 echo -e "\n"
-# echo "Synology nfs shares"
-# echo -e "\n"
+echo "Nfs shares and other config"
+echo -e "\n"
 
-#NFS mounts:
-#sudo systemctl enable --now nfs-client.target
-#sudo systemctl enable --now NetworkManager-wait-online.service
-#showmount -e 192.168.77.18
-
-#sudo mkdir -p /data/nfs/FilmyNas /data/nfs/HudbaNas /data/nfs/Nas &&
-#sudo chmod -R ugo+rwx /data/nfs
-#sudo mount -t nfs 192.168.77.18:/volume1/Rodinas /data/nfs/Nas
-#sudo mount -t nfs 192.168.77.18:/volume1/Hudba /data/nfs/HudbaNas
-#sudo mount -t nfs 192.168.77.18:/volume1/Filmy /data/nfs/FilmyNas
-
-##add to fstab:
-#sudo cp /etc/fstab /etc/fstab.bak
-#cd ~/.dotfiles/scripts/
-#cat 3fstabnfs | sudo tee -a /etc/fstab
-#echo -e "\n"
-
-#echo "Připojení Nas Synology proběhlo úspěšně"
-#echo "Soubor fstab nyní vypadá takto:"
-#echo "-------------------------------------------------------------------------"
-#echo -e "\n"
-#cat /etc/fstab
-
-#echo -e "\n"
-#echo "-------------------------------------------------------------------------"
+# NFS mounts and grub and sddm conf:
+sudo cp -rf /home/libor/.dotfiles/other/to-etc/auto.master auto.nfs sddm.conf.d/ /etc/
+sudo cp -rf /home/libor/.dotfiles/other/to-etc/systemd/sleep.conf.d /etc/systemd/
+sudo cp -rf /home/libor/.dotfiles/other/to-etc/grub.d/40_custom /etc/grub.d/
 
 # git@github.com:lrestj/debian.git
+# git@gitlab.com:lrestj/debian.git
 # ssh://git@codeberg.org/lrestj/debian.git
-
 git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote remove origin
 git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add github git@github.com:lrestj/debian.git
 git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add gitlab git@gitlab.com:lrestj/debian.git
@@ -61,7 +40,7 @@ git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor remote add codeberg 
 git config --global user.email "rest@seznam.cz"
 git config --global user.name "LrestJ"
 
-echo "Codeberg and Github remote repos added"
+echo "Remote repos added"
 echo -e "\n"
 echo "Instalace kompletní" 
 
