e6ceca81a44cb1114a41fd6267dfa73c8a8d2d21
LrestJ <rest@seznam.cz>
2025-07-13 20:25:32 +0200

    Fixes

--- .dotfiles/scripts/kbdlayout.sh
+++ .dotfiles/scripts/kbdlayout.sh
@@ -2,5 +2,5 @@
 keyboard="at-translated-set-2-keyboard"
 hyprctl switchxkblayout $keyboard next
 value=$(hyprctl devices | grep -i $keyboard -A 2 | tail -n1 | cut -f3,4 -d' ')
-notify-send "Rozložení klávesnice ${value}"
+notify-send -t 1000 "Rozložení klávesnice ${value}"
 
